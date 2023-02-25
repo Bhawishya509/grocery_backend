11be1eccb2c3babae7b0c771dbebb7fc9ea88082
Bhawishya Singh <102045314+Bhawishya509@users.noreply.github.com>
2023-02-26 00:16:45 +0530

    second

--- Index.js
+++ Index.js
@@ -1,12 +1,13 @@
 import express from "express"
 import Route from "./Route/Route.js"
 const app=express()
+const PORT=process.env.PORT||8000;
 import cors from "cors"
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(Route)
-app.listen(8000,()=>
+app.listen(PORT,()=>
 {
     console.log("server is runing")
 })
